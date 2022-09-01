406952c6d4868d570880a8c4f054532127d6eaec
leejiwon <365support@naver.com>
2022-09-01 17:53:08 +0900

    first commit

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery, useMutation } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,15 +25,50 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError } = useQuery(["comments", post.id], () =>
+    fetchComments(post.id)
+  );
+
+  const deleteMutaion = useMutation(() => deletePost(post.id));
+  const updateMutaion = useMutation(() => updatePost(post.id));
+
+  // const deleteMutaion = useMutation((postId) => deletePost(postId));
+  // useMutation 에서는 그 자체도 인수를 받아서 전달 할 수 있다
+
+  if (isLoading) return <div>Loading</div>;
+  if (isError) return <div>Error</div>;
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutaion.mutate(post.id)}>Delete</button>
+      <button onClick={() => updateMutaion.mutate(post.id)}>
+        Update title
+      </button>
+      {deleteMutaion.isError && (
+        <p style={{ color: "red" }}> Error deleting the post</p>
+      )}
+      {deleteMutaion.isLoading && (
+        <p style={{ color: "purple" }}> Deleting the post</p>
+      )}
+      {deleteMutaion.isSuccess && (
+        <p style={{ color: "green" }}>Post has (not) been deleted</p>
+      )}
+      {updateMutaion.isError && (
+        <p style={{ color: "red" }}> updateMutaion Error deleting the post</p>
+      )}
+      {updateMutaion.isLoading && (
+        <p style={{ color: "purple" }}> updateMutaion Deleting the post</p>
+      )}
+      {updateMutaion.isSuccess && (
+        <p style={{ color: "green" }}>
+          {" "}
+          updateMutaion Post has (not) been deleted
+        </p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data?.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const queryClient = useQueryClient();
 
+  // 다음 페이지를 미리 누를 걸 예상해서 prefetchQuery 미리 데이터 가져오기
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["post", nextPage], () => fetchPosts(nextPage));
+    }
+  }, [currentPage, queryClient]);
+
+  // replace with useQuery
+  const { data, isError, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      keepPreviousData: true,
+    }
+  );
+  // 쿼리 키, 쿼리함수 (쿼리에 대한 데이터를 가져오는 방법, 데이터를 가져오는 비동기 함수여야 한다)
+  if (isLoading) return <div>Loading</div>;
+  if (isError) return <div>Error</div>;
+
+  // isFetching의 경우 async 쿼리 함수가 해결되지 않았을 떄 참에 해당 됨 -> 아직 데이터를 가져오는 중
+  // isLoading의 경우 isFetching이 참이면서 쿼리에 캐시된 데이터가 없는 상태
   return (
     <>
       <ul>
@@ -31,11 +53,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
           Next page
         </button>
       </div>
